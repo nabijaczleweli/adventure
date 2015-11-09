5d614aef167a9f89394a316141bada05bf6328aa
Julian Torreno <Skorezore@gmail.com>
2015-11-08 20:50:23 -0500

    Stop sucking at C++.

--- source/main.cpp
+++ source/main.cpp
@@ -15,14 +15,17 @@ int main()
 
     while (true)
     {
-        switch(stream_get<char>(std::cin))
+        if (stream_get<char>(std::cin) == 'y')
         {
-            case 'y': std::cout << "Too bad; you aren't getting one. This game's no where near finished. Not even by Ubisoft's standards." << std::endl;
-                      break;
-            case 'n': std::cout << "Oh, alright." << std::endl;
-                      break;
-
-            default: std::cout << "Were the instructions unclear or are you just of the rebellious kind? Try again [y/n]: ";
+            std::cout << "Too bad; you aren't getting one. This game's no where near finished. Not even by Ubisoft's standards." << std::endl;
+            break;
         }
+        else if (stream_get<char>(std::cin) == 'n')
+        {
+            std::cout << "Oh, alright." << std::endl;
+            break;
+        }
+
+        std::cout << "Were the instructions unclear or are you just of the rebellious kind? Try again [y/n]: ";
     }
 }
