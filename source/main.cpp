05899b8b6e922ace04bd07696664e0756f8e1697
nabijaczleweli <nabijaczleweli@gmail.com>
2015-11-11 19:50:47 +0100

    Be a little bit sane about reading user-input

--- source/main.cpp
+++ source/main.cpp
@@ -15,17 +15,19 @@ int main()
 
     while (true)
     {
-        if (stream_get<char>(std::cin) == 'y')
+        const auto response = stream_get<char>(std::cin);
+        if (response == 'y')
         {
-            std::cout << "Too bad; you aren't getting one. This game's no where near finished. Not even by Ubisoft's standards." << std::endl;
+            std::cout << "Too bad; you aren't getting one. This game's no where near finished. Not even by Ubisoft's standards.";
             break;
         }
-        else if (stream_get<char>(std::cin) == 'n')
+        else if (response == 'n')
         {
-            std::cout << "Oh, alright." << std::endl;
+            std::cout << "Oh, alright.";
             break;
+        } else
+        {
+            std::cout << "Were the instructions unclear or are you just of the rebellious kind? Try again [y/n]: ";
         }
-
-        std::cout << "Were the instructions unclear or are you just of the rebellious kind? Try again [y/n]: ";
     }
 }
